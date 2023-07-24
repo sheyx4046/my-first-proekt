385a2b45c8b4a6ab2b23e9c22aad2c147e855b75
sheyx4046 <sherxonyuldoshevsherxon@gmail.com>
2023-07-24 11:56:01 +0500

    first commint

---
+++ src/main/java/com/example/springwihtdocker/SpringWihtDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwihtdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWihtDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWihtDockerApplication.class, args);
+    }
+
+}
